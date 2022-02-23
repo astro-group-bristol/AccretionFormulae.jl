d133be3eb4185be8f4387e366d29aad909f06788
fjebaker <fergusbkr@gmail.com>
2022-02-23 16:32:22 +0000

    bump version

--- src/AccretionFormulae.jl
+++ src/AccretionFormulae.jl
@@ -3,6 +3,8 @@ module AccretionFormulae
 import GeodesicBase: AbstractMetricParams
 import CarterBoyerLindquist: CarterMethodBL, Σ, Δ, A
 
+import StaticArrays: @SVector
+
 include("redshift.jl")
 include("emission_profile.jl")
 
