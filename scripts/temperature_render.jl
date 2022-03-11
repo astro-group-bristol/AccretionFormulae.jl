9dbefd22bc679e994091a90996233ba66c05373e
dangoh123 <daniel.goh@hotmail.co.uk>
2022-03-11 16:51:34 +0000

    Fixed Units and Mass Scaling
    Corrected the the temperature that we got from a 1kg black hole, and scaled it up to an arbitrary mass black hole. This mass is measured in solar masses, and is calculated after the initial image is generated.
    
    Also made some changes to the units which fixed some of the other scaling issues, so everything should all be correct now.
    
    Removed a few old comments, and added a few `kwargs` to the `temperature_render()` function.
    
    Also added an optional autoscaler which is commented out at the moment, so that the scale stays consistent for different masses.

--- scripts/temperature_render.jl
+++ scripts/temperature_render.jl
@@ -11,64 +11,96 @@ gr()
 function temperature(m, sol, max_time; kwargs...)
     g =  AccretionFormulae.redshift(m, sol, max_time; kwargs...)
 
-    u = sol.u[end] # this should be in the same units of M as r_isco?
-
-    M = m.M*1.99e30 # this doesn't really make sense and should be removed
-    a = m.a
-    a_star = a/M # this shouldn't be needed, as a should already be dimensionless?
+    u = sol.u[end]
+    M = m.M
+    a_star = m.a
     temperature = AccretionFormulae.observed_temperature(u[2], a_star, M, g)
+    
 end
 
-function temperature_render(mass=1.0, spin=0.998;obs_angle=85.0, disc_angle=90.0, 
+function radius(m, sol, max_time; kwargs...)
+    u = sol.u[end]
+    radius = u[2]
+end
+
+function temperature_render(;mass=1, spin=0.998, obs_angle=85.0, disc_angle=90.0, 
                             tolerance=1e-8, dtmax=1000.0, 
-                            size_multiplier::Int64=1, resolution=400, fov=3.0
+                            size_multiplier::Int64=1, resolution=400, fov=3.0,
+                            η=0.1, η_phys=0.1, edd_ratio=0.1, edd_ratio_phys=0.1
                             )
                             
-    m = CarterMethodBL(M=mass, a=spin)
+    m = CarterMethodBL(M=1.0, a=spin)
+    M = m.M
 
     # observer position
     u = [0.0, 1000.0, deg2rad(obs_angle), 0.0]
     R_isco = AccretionFormulae.r_isco(m.a, m.M)
 
-    # this should work now, as when we tested it, both te AccretionFormulae 
-    # and CarterBoyerLindquist R_isco functions gave the same values when the 
-    # input values are actually the same:
-    # R_isco = CarterBoyerLindquist.rms(m.M, m.a)
-
-    # disc has inner radius 10, outer radius 50, perpendicular to the spin axis
-    d = GeometricThinDisc(R_isco, 50.0, deg2rad(disc_angle))
+    # disc
+    d = GeometricThinDisc(R_isco+1, 50.0, deg2rad(disc_angle))
 
 
-    # create and compose the ValueFunction
-    redshift_vf = (
-        # calculate redshift
+    # create and compose the ValueFunctions
+    temperature_vf = (
         ValueFunction(temperature)
-        # filter only pixels with r > 9, i.e. on the disc
         ∘ FilterValueFunction((m, sol, max_time; kwargs...) -> sol.u[end][2] > R_isco, NaN)
-        # filter only pixels that terminated early (i.e., intersected with something)
+        ∘ ConstValueFunctions.filter_early_term
+    )
+
+    radius_vf = (
+        ValueFunction(radius)
+        ∘ FilterValueFunction((m, sol, max_time; kwargs...) -> sol.u[end][2] > R_isco, NaN)
         ∘ ConstValueFunctions.filter_early_term
     )
 
     # do the render
-    img = @time rendergeodesics(
+    temperature_img = @time rendergeodesics(
         m, u, 2000.0, 
         d, 
         fov_factor=fov*size_multiplier, abstol=tolerance, reltol=tolerance,
         image_width = 350*size_multiplier,
         image_height = 250*size_multiplier,
-        vf = redshift_vf,
+        vf = temperature_vf,
         dtmax = dtmax
     )
 
-    # plot
-    scale = 1e4
+    radius_img = @time rendergeodesics(
+        m, u, 2000.0, 
+        d, 
+        fov_factor=fov*size_multiplier, abstol=tolerance, reltol=tolerance,
+        image_width = 350*size_multiplier,
+        image_height = 250*size_multiplier,
+        vf = radius_vf,
+        dtmax = dtmax
+    )
+
+    # correcting for physical mass
+    M_phys = mass*1.99e30
+    r_isco_phys = AccretionFormulae.r_isco(M_phys, 0.998)
+    r_g_phys = M_phys
+
+    numerators =   AccretionFormulae.mass_scale_fraction.(M_phys, η_phys, edd_ratio_phys, r_isco_phys, r_g_phys, radius_img*M_phys)
+    denominators = AccretionFormulae.mass_scale_fraction.(M, η, edd_ratio, R_isco, M, radius_img)
+
+    fractions = numerators./denominators
+    temperature_img .*= fractions
+
+    # # autoscale
+    # scale = maximum(filter(!isnan,temperature_img))
+    # scale = floor(log(10, scale))
+    # scale = 10^scale
+    
+    # scaling image
+    scale = 1e7
     scalestr = @sprintf "%.E" scale
-    new_img = reverse(img, dims=1)
+    new_img = reverse(temperature_img, dims=1)
     new_img ./= scale
 
-    heatmap(new_img, aspect_ratio=1.0, size=(resolution*3/2, resolution), clim=(0,40))
-    # contour(new_img, aspect_ratio=1.0, size=(resolution*3/2, resolution), clim=(0,40))
-    title!("Temperature Scale = $scalestr, Relative Mass = $(m.M)")
+    heatmap(new_img, aspect_ratio=1.0, size=(resolution*3/2, resolution), 
+    clim=(0,3)
+    )
+    # contour(new_img, aspect_ratio=1.0, size=(resolution*3/2, resolution), clim=(0,3))
+    title!("Temperature Scale = $scalestr, Mass = $mass M_☼")
 end
 
-temperature_render(obs_angle=85.0)
+temperature_render(obs_angle=85.0, mass=1)
--- src/emission_profile.jl
+++ src/emission_profile.jl
@@ -1,4 +1,3 @@
-#functions
 """
 From Reynolds (2020) (Eq2-4)
 Calculates the radius of the inner most stable circular orbit of the a black hole,
@@ -6,8 +5,7 @@ with spin a_star, and mass M.
 """
 function r_isco(a_star, M)
     a = a_star
-    r_g = 2*G*M/(c^2) # this should be removed
-    # r_g = M This will be what is used once the eddington limit is sorted. 
+    r_g = M 
     z1 = 1+∛(1-a^2)*(∛(1+a)+∛(1-a))
     z2 = √(3*a^2+z1^2)
     if a >= 0
@@ -23,7 +21,6 @@ Calculates the function f at radius r, for a black hole of spin a_star, and mass
 which is used to calculate the flux given by its accretion disk.
 """
 function flux(r, a_star, M)
-
     R_isco = r_isco(a_star, M)
     x = √(r/M)
     x0 = √(R_isco/M)
@@ -38,20 +35,13 @@ function flux(r, a_star, M)
 end
 
 function mdot(M)
-    # need something here to convert M into SI units, as M_☼ is in SI, althought we 
-    # aren't currently clear about what the units M currently is in
-    # would look something like:
-    # M_kg = M*c^2
-    # L_edd = 3e4*L_☼*(M_kg/M_☼)
-
     L_edd = 3e4*L_☼*(M/M_☼)
     Mdot = -L_edd/(c^2*η)
     mdot = -0.1*Mdot
 end
 
 function diss(mdot, r, a_star, M)
-    # diss = ((c^6)/(G^2))*mdot*flux(r, a_star, M)/(4*π*r) Will add this once mdot is fixed, we found this prefactor using dimensional analysis
-    diss = mdot*flux(r, a_star, M)/(4*π*r)
+    diss = ((c^6)/(G^2))*mdot*flux(r, a_star, M)/(4*π*r)
 end
 
 function temperature(r, a_star, M)
@@ -67,6 +57,10 @@ function observed_temperature(r, a_star, M, g)
     observed_temperature =  g*T
 end
 
+function mass_scale_fraction(M, η, edd_ratio, r_isco, r_g, r)
+    mass_scale_fraction = ((M * η)^(-1) * (edd_ratio * (1-√(r_isco/r))) * (r_g/r)^3)^(1/4)
+end
+
 # constants
 G = 6.67e-11
 c = 3e8
@@ -75,4 +69,4 @@ M_☼ = 1.99e30
 σ_SB = 5.67e-8
 η = 0.1
 
-export observed_temperature, r_isco, temperature
+export observed_temperature, r_isco, temperature, mass_scale_fraction
