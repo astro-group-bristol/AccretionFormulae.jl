bee89f2393acc500b58418d0e513533f41ee4e5b
fjebaker <fergusbkr@gmail.com>
2022-03-16 18:50:45 +0000

    julia formatting

--- scripts/histogram_test.jl
+++ scripts/histogram_test.jl
@@ -9,25 +9,35 @@ using Plots
 gr()
 
 function temperature(m, gp, max_time; kwargs...)
-    g =  AccretionFormulae.redshift(m, gp, max_time; kwargs...)
+    g = AccretionFormulae.redshift(m, gp, max_time; kwargs...)
 
     M = m.M
     a_star = m.a
     temperature = AccretionFormulae.observed_temperature(gp.u[2], a_star, M, g)
-    
+
 end
 
 function radius(m, gp, max_time; kwargs...)
     gp.u[2]
 end
 
-function temperature_render(;mass=1, spin=0.998, obs_angle=85.0, disc_angle=90.0, 
-                            tolerance=1e-8, dtmax=1000.0, 
-                            size_multiplier::Int64=1, resolution=400, fov=3.0,
-                            η=0.1, η_phys=0.1, edd_ratio=0.1, edd_ratio_phys=0.1
-                            )
-                            
-    m = CarterMethodBL(M=1.0, a=spin)
+function temperature_render(;
+    mass = 1,
+    spin = 0.998,
+    obs_angle = 85.0,
+    disc_angle = 90.0,
+    tolerance = 1e-8,
+    dtmax = 1000.0,
+    size_multiplier::Int64 = 1,
+    resolution = 400,
+    fov = 3.0,
+    η = 0.1,
+    η_phys = 0.1,
+    edd_ratio = 0.1,
+    edd_ratio_phys = 0.1,
+)
+
+    m = CarterMethodBL(M = 1.0, a = spin)
     M = m.M
 
     # observer position
@@ -35,63 +45,76 @@ function temperature_render(;mass=1, spin=0.998, obs_angle=85.0, disc_angle=90.0
     R_isco = AccretionFormulae.r_isco(m.a, m.M)
 
     # disc
-    d = GeometricThinDisc(R_isco+1, 50.0, deg2rad(disc_angle))
+    d = GeometricThinDisc(R_isco + 1, 50.0, deg2rad(disc_angle))
 
 
     # cache the render
     cache = @time prerendergeodesics(
-        m, u, 2000.0, 
-        d, 
-        fov_factor=fov*size_multiplier, abstol=tolerance, reltol=tolerance,
-        image_width = 350*size_multiplier,
-        image_height = 350*size_multiplier,
-        dtmax = dtmax
+        m,
+        u,
+        2000.0,
+        d,
+        fov_factor = fov * size_multiplier,
+        abstol = tolerance,
+        reltol = tolerance,
+        image_width = 350 * size_multiplier,
+        image_height = 350 * size_multiplier,
+        dtmax = dtmax,
     )
 
     # create and compose the PointFunctions
     temperature_vf = (
-        PointFunction(temperature)
-        ∘ FilterPointFunction((m, gp, max_time; kwargs...) -> gp.u[2] > R_isco, NaN)
-        ∘ ConstPointFunctions.filter_early_term
+        PointFunction(temperature) ∘
+        FilterPointFunction((m, gp, max_time; kwargs...) -> gp.u[2] > R_isco, NaN) ∘
+        ConstPointFunctions.filter_early_term
     )
 
     radius_vf = (
-        PointFunction(radius)
-        ∘ FilterPointFunction((m, gp, max_time; kwargs...) -> gp.u[2] > R_isco, NaN)
-        ∘ ConstPointFunctions.filter_early_term
+        PointFunction(radius) ∘
+        FilterPointFunction((m, gp, max_time; kwargs...) -> gp.u[2] > R_isco, NaN) ∘
+        ConstPointFunctions.filter_early_term
     )
 
     redshift_vf = (
-        PointFunction(AccretionFormulae.redshift)
-        ∘ FilterPointFunction((m, gp, max_time; kwargs...) -> gp.u[2] > R_isco, NaN)
-        ∘ ConstPointFunctions.filter_early_term   
+        PointFunction(AccretionFormulae.redshift) ∘
+        FilterPointFunction((m, gp, max_time; kwargs...) -> gp.u[2] > R_isco, NaN) ∘
+        ConstPointFunctions.filter_early_term
     )
 
     radius_img = GeodesicRendering.apply(radius_vf, cache)
     temperature_img = GeodesicRendering.apply(temperature_vf, cache)
     redshift_img = GeodesicRendering.apply(redshift_vf, cache)
 
-    
+
     # correcting for physical mass
-    M_phys = mass*1.99e30
+    M_phys = mass * 1.99e30
     r_isco_phys = AccretionFormulae.r_isco(M_phys, 0.998)
     r_g_phys = M_phys
 
-    numerators =   AccretionFormulae.mass_scale_fraction.(M_phys, η_phys, edd_ratio_phys, r_isco_phys, r_g_phys, radius_img*M_phys)
-    denominators = AccretionFormulae.mass_scale_fraction.(M, η, edd_ratio, R_isco, M, radius_img)
+    numerators =
+        AccretionFormulae.mass_scale_fraction.(
+            M_phys,
+            η_phys,
+            edd_ratio_phys,
+            r_isco_phys,
+            r_g_phys,
+            radius_img * M_phys,
+        )
+    denominators =
+        AccretionFormulae.mass_scale_fraction.(M, η, edd_ratio, R_isco, M, radius_img)
 
-    fractions = numerators./denominators
+    fractions = numerators ./ denominators
     temperature_img .*= fractions
 
     # # autoscale
     # scale = maximum(filter(!isnan,temperature_img))
     # scale = floor(log(10, scale))
     # scale = 10^scale
-    
+
     # scaling image
     scale = 1e7
     scalestr = @sprintf "%.E" scale
-    new_img = reverse(temperature_img, dims=1)
+    new_img = reverse(temperature_img, dims = 1)
     new_img ./= scale
 
     # hm = heatmap(new_img, aspect_ratio=1.0, size=(resolution*3/2, resolution), 
@@ -104,8 +127,13 @@ function temperature_render(;mass=1, spin=0.998, obs_angle=85.0, disc_angle=90.0
 end
 
 
-function energy_histogram(;obs_angle=30.0)
-    temperature_img, redshift_img, radius_img = temperature_render(obs_angle=obs_angle, size_multiplier=2, fov=6.0, tolerance=1.0E-9)
+function energy_histogram(; obs_angle = 30.0)
+    temperature_img, redshift_img, radius_img = temperature_render(
+        obs_angle = obs_angle,
+        size_multiplier = 2,
+        fov = 6.0,
+        tolerance = 1.0E-9,
+    )
 
     # Define an energy array and a line profile array
     n_energies = 100
@@ -113,7 +141,7 @@ function energy_histogram(;obs_angle=30.0)
     e_min = 1.0
     # Maximum energy in keV
     e_max = 10.0
-    energies = range(e_min, stop=e_max, length=n_energies)
+    energies = range(e_min, stop = e_max, length = n_energies)
     lineProfile = zeros(n_energies)
 
     # Define inner and outer radius of disk (could do this direclty using GeometricThinDisc)
@@ -125,7 +153,7 @@ function energy_histogram(;obs_angle=30.0)
             g = redshift_img[i][j]
             if !isnan(g)
                 en = 6.4 * g
-                index = trunc(Int, 1 + (en-e_min)*(n_energies-1)/(e_max-e_min))
+                index = trunc(Int, 1 + (en - e_min) * (n_energies - 1) / (e_max - e_min))
                 if (index >= 1) && (index <= n_energies)
                     # Example line profile where the emissivity is proportional to radius^-3 (an arbitrary choice)
                     if (radius_img[i][j] > r_in) && (radius_img[i][j] < r_out)
@@ -137,7 +165,13 @@ function energy_histogram(;obs_angle=30.0)
     end
 
     # plot line profile
-    plot(energies, lineProfile, label=string(obs_angle), xlabel="Energy", ylabel="Line flux (arbitrary units)")
+    plot(
+        energies,
+        lineProfile,
+        label = string(obs_angle),
+        xlabel = "Energy",
+        ylabel = "Line flux (arbitrary units)",
+    )
 
     # plot r image
     # heatmap(radius_img, aspect_ratio=1.0, clim=(0,50))
