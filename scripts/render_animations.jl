9dfda8835958162ae46e0e575201462dd749fd60
dangoh123 <daniel.goh@hotmail.co.uk>
2022-03-17 20:16:19 +0000

    Changing Animations For Solva

--- scripts/render_animations.jl
+++ scripts/render_animations.jl
@@ -3,7 +3,8 @@ include("temperature_render.jl")
 # choosing the range to animate over
 # vars = 75.0:1.0:85.0 # changing angle small
 # vars = [-179.0:1.0:-1.0;1.0:1.0:179.0] # changing angle full
-vars = 1.0:5.0:200 # changing mass
+# vars = 1.0:5.0:200 # changing mass
+vars = 0:0.1:0.998
 
 # generating frames
 n_frames = length(vars)
@@ -11,11 +12,11 @@ anim = @animate for (i, var) in enumerate(vars)
     print("Frame: $i / $n_frames complete")
     fvar = Float64(var)
     temperature_render(
-                        mass=var, 
-                        tolerance=1e-10, 
+                        mass=10,
+                        spin=var, 
+                        tolerance=1e-8, 
                         resolution=1080, 
-                        size_multiplier=4, 
-                        dtmax=2.5
+                        size_multiplier=2
                         )
 end
 
