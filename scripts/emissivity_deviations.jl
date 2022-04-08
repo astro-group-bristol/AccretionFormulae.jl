aba17e0a86e2cc48a90cfeb63e9db2d496b1309e
dangoh123 <daniel.goh@hotmail.co.uk>
2022-04-08 01:07:34 +0100

    Updated Emissivity Deivations

--- scripts/emissivity_deviations.jl
+++ scripts/emissivity_deviations.jl
@@ -1,6 +1,6 @@
 using Plots
 using QuadGK
-using 
+using GeodesicBase
 
 α_13 = 0
 α_22 = 0
@@ -11,7 +11,8 @@ using
 
 M = 1.99e30
 r = M
-a = 0.0
+vϕ_vals = []
+# a = 0.0
 
 # Johannsen 2014 eq 28
 function gsqrt(ϵ_3)
@@ -22,8 +23,18 @@ function gsqrt(ϵ_3)
                 (1 + (α_52 * M^2)/(r^2)))
 end
 
-function E()
-E = 1
+u = [0, init_radius, π/2, 0]
+vs = [@SVector [0.0, 0.0, 0.0, -vϕ/100] for vϕ in vϕ_vals]
+
+m = CarterMethodBL(M = 1.0, a = 0.998)
+
+for v in vs
+    En = GeodesicBase.E(m, u, v)
+    L_z = GeodesicBase.Lz(m, u, v)
+
+    values = (r, v[4], En, L_z)
+    
+
 Ω = 0.1
 L_z = 1
 
