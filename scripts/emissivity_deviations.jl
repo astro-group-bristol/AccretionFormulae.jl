b98980f0301eb0c7869d5f733df206511d178fcd
Dan Goh <daniel.goh@hotmail.co.uk>
2022-04-08 19:46:10 +0100

    Added an emission profile for an arbitrary metric
    Works by finding geodesics, then using eq 27 from Johannsen (2014).
    Works to a reasonable degree so far, but has negative values, and is
    quite noisy and slow.

--- scripts/emissivity_deviations.jl
+++ scripts/emissivity_deviations.jl
@@ -1,6 +1,12 @@
 using Plots
 using QuadGK
 using GeodesicBase
+using CarterBoyerLindquist
+using ComputedGeodesicEquations
+using GeodesicTracer
+using Statistics
+using Interpolations
+
 include("geodesic_search.jl")
 
 α_13 = 0
@@ -10,13 +16,44 @@ include("geodesic_search.jl")
 
 ϵ_3 = 0
 
-M = 1.99e30
-r_vals, vϕ_vals = search()
+M = 1
+R_isco = AccretionFormulae.r_isco(0.998, 1)
 
-# a = 0.0
+# getting solutions from the search
+r_vals_long, _, r_dict_vϕ, r_dict_Lz, r_dict_En = search(
+                                                        init_radii = R_isco+1:0.05:30,
+                                                        vϕ_vals = 0.00:0.001:10.00
+                                                        )
 
-# Johannsen 2014 eq 28
-function gsqrt(ϵ_3)
+# setting up unique solution lists
+vϕ_vals = []
+Lz_vals = []
+En_vals = []
+r_vals = []
+
+# sorting dictionaries
+r_dict_vϕ = sort(r_dict_vϕ)
+r_dict_Lz = sort(r_dict_Lz)
+r_dict_En = sort(r_dict_En)
+
+# averaging repeat solutions to find the gradients
+for init_radius in keys(r_dict_vϕ)
+    if length(r_dict_vϕ[init_radius]) != 0
+        push!(vϕ_vals,  mean(r_dict_vϕ[init_radius]))
+        push!(Lz_vals,  mean(r_dict_Lz[init_radius]))
+        push!(En_vals,  mean(r_dict_En[init_radius]))
+        push!(r_vals, init_radius)
+    end
+end
+
+# finding gradients of relevant values
+itp_vϕ = interpolate((r_vals,), vϕ_vals, Gridded(Linear()));
+itp_Lz = interpolate((r_vals,), Lz_vals, Gridded(Linear()));
+
+"""
+Johannsen 2014 eq 28
+"""
+function gsqrt(ϵ_3, r, a)
     gsqrt =   (r/abs(1 + (α_13 * M^3)/(r^3) -  
                         (α_22 * a^2 * M^2)/(r^4) + 
                         (α_13 * a^2 * M^3)/(r^5))) *
@@ -24,32 +61,57 @@ function gsqrt(ϵ_3)
                 (1 + (α_52 * M^2)/(r^2)))
 end
 
-for init_radius in r_vals
-    u = [0, init_radius, π/2, 0]
-    vs = [@SVector [0.0, 0.0, 0.0, -vϕ/100] for vϕ in vϕ_vals]
-
-    m = CarterMethodBL(M = 1.0, a = 0.998)
+f_vals = []
+for (i, init_radius) in enumerate(r_vals)
+    # vϕ_vals = r_dict_vϕ[init_radius]
+    # u = [0, init_radius, π/2, 0]
+    # vs = [@SVector [0.0, 0.0, 0.0, -vϕ/100] for vϕ in r_dict_vϕ[init_radius]]
+    # m = BoyerLindquist(M = 1.0, a = 0.998)
 
     solutions = [] # all different combos of solutions for each r value
 
-    for v in vs
+    # for v in vs
+    #     @show(v)
         # En = GeodesicBase.E(m, u, v)
         # L_z = GeodesicBase.Lz(m, u, v)
+        # push!(L_z_vals, L_z)
+    En = En_vals[i]
+    Lz = Lz_vals[i]
+    vϕ = vϕ_vals[i]
+    g_factor = gsqrt(0, init_radius, 0.998)
+    M = 1
+    dΩdr = only(Interpolations.gradient(itp_vϕ, init_radius))
+    dLzdr = only(Interpolations.gradient(itp_Lz, init_radius))
 
-        En=10
-        L_z=10
+    #     values = (init_radius, v[4], En, L_z)
+    #     push!(solutions, values)
+    # # end
+    
+    # @show(solutions)
 
-        values = (r, v[4], En, L_z)
-    end
-        
 
-    Ω = 0.1
-    L_z = 1
+    pt1 = dΩdr
+    pt2 = (M^2)/(g_factor*(En - vϕ*Lz)^2)
+    pt3 = quadgk(x -> ((En-vϕ*Lz)*dLzdr), init_radius, R_isco, rtol=1e-3)
 
-    pt1 = 0
-    pt2 = (M^2)/(gsqrt(ϵ_3)*(E - Ω*L_z))
-    pt3 = quadgk(x -> ((α*x^3)/(β*x*2 - (α - x)^2)^(1/2)), r_isco, r, rtol=1e-3)
+    @show(pt1)
+    @show(pt2)
+    @show(pt3)
 
-    f_disk = pt1 + pt2 + pt3
-    print(f_disk)
+    f_disk = -(pt1 * pt2 * pt3[1])
+    push!(f_vals, f_disk)
 end
+# f_vals .^= (1/4)
+plt = plot(r_vals, f_vals)
+@show(plt)
+# L_z_vals_mean = []
+# for init_radius in keys(r_dict_vϕ)
+#     push!(L_z_vals_mean,  mean(r_dict_vϕ[init_radius]))
+#     push!(r_vals, init_radius)
+# end
+
+# itp_L_z = interpolate((r_vals,), vϕ_vals, Gridded(Linear()));
+
+
+# plt = plot(r_vals_long, L_z_vals)
+# display(plt)
--- scripts/geodesic_search.jl
+++ scripts/geodesic_search.jl
@@ -5,26 +5,39 @@ using StaticArrays
 using Plots
 using AccretionFormulae
 using Dates
+
+using GeodesicBase
+using CarterBoyerLindquist
 gr()
 
 function search(;
                 init_radii = 10:0.1:20,
                 vϕ_vals = 1.00:0.01:10.00
                 )       
+    # values to test
     init_radii = init_radii
     vϕ_vals = vϕ_vals
-    r_dict = Dict()
+
+    # dictionaries for the multiple solutions of each result at each radius
+    r_dict_vϕ = Dict()
+    r_dict_Lz = Dict()
+    r_dict_En = Dict()
     for init_radius in init_radii
-        r_dict[init_radius] = []
+        r_dict_vϕ[init_radius] = []
+        r_dict_Lz[init_radius] = []
+        r_dict_En[init_radius] = []
     end
+
     total = length(init_radii)*length(vϕ_vals) # total number of combinations
     found = 0   # number of combinations found
-    r_vals=[]
+    r_vals=[]   # found vals (maybe not needed)
     v_vals=[]
+    i = 0 # current test
 
     start = now() # timing start
     for init_radius in init_radii
-
+                    
+        println("Testing Radius: $i / $(length(init_radii))")
         m = BoyerLindquist(M=1.0, a=1.0)
         R_isco = AccretionFormulae.r_isco(m.a, m.M)
         u = @SVector [0.0, init_radius, deg2rad(90.0), 0.0]
@@ -55,16 +68,28 @@ function search(;
             
             # if the difference is small enough, and it doesn't crash into the ISCO
             # it is a valid solution, and is displayed
-            if (diff <= 1) & (min > R_isco*1.1)
+            if (diff <= 0.05) & (min > R_isco*1.1)
                 found +=1
                 # plt = plot(sol, vars=(8, 6), projection=:polar, range=(0, 21), legend=false)
                 # title!("Initial Radius=$init_radius, vϕ=$(vϕ_vals[j])")
                 # display(plt)
+                vϕ = vϕ_vals[j]
+                u = [0, init_radius, π/2, 0]
+                v = @SVector [0.0, 0.0, 0.0, -vϕ/100]
+                m = BoyerLindquist(M = 1.0, a = 0.998)
+                
+                En = GeodesicBase.E(m, u, v)
+                Lz = GeodesicBase.Lz(m, u, v)
+
                 push!(r_vals, init_radius)
-                push!(v_vals, vϕ_vals[j])
-                push!(r_dict[init_radius], vϕ_vals[j])
+                push!(v_vals, vϕ)
+                push!(r_dict_vϕ[init_radius], vϕ)
+                push!(r_dict_Lz[init_radius], Lz)
+                push!(r_dict_En[init_radius], En)
             end
+            
         end
+        i += 1
     end
     duration = now() - start # timing finish
     println("Done")
@@ -74,5 +99,5 @@ function search(;
     # r_vals, v_vals = collect(zip(stable_orbits))
     plt = plot(r_vals, v_vals, xlabel="Initial Radius", ylabel="vϕ")
     display(plt)
-return r_vals, v_vals, r_dict
+return r_vals, v_vals, r_dict_vϕ, r_dict_Lz, r_dict_En
 end
