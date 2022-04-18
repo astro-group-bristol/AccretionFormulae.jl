611a0dde2f124b8ed81792225ba4be5bbf0909f9
Dan Goh <daniel.goh@hotmail.co.uk>
2022-04-18 21:07:31 +0100

    Changed Values

--- scripts/solva_plots/PT/iron_lines/iron_heatmap.jl
+++ scripts/solva_plots/PT/iron_lines/iron_heatmap.jl
@@ -3,8 +3,8 @@ include("../../../good_plots/iron_line_heatmap.jl")
 vars = LinRange(5.0, 85.0, 500)
 
 hmp = iron_line_heatmap(tolerance=1e-12,
-                        dtmax=0.5,
-                        size_multiplier=10,
-                        nbins=400,
+                        dtmax=5,
+                        size_multiplier=5,
+                        nbins=300,
                         vars=vars)
 png(hmp, "iron_heatmap.png")
